35ee3dd008d2d94832546a6e12a55a0fd958985c
Ahmad-U19 <ahmedoppal2592@gmail.com>
2024-12-17 22:18:08 +0500

    Final Code

---
+++ script.js
@@ -0,0 +1,52 @@
+var str="I am Ahmad Hassaan";
+var a=str.length;
+
+document.write(a);
+
+var b=str.toLowerCase();
+
+document.write("<br>"+b);
+
+var c=str.toUpperCase();
+
+document.write("<br>"+c);
+
+var d=str.includes("Ahmad");
+
+document.write("<br>"+d);
+
+var e=str.startsWith("I");
+
+document.write("<br>"+e);
+
+var f=str.endsWith("Hassaan");
+
+document.write("<br>"+f);
+
+var g=str.search("am");
+
+document.write("<br>"+g);
+
+var h=str.match("Wasif");
+
+document.write("<br>"+h);
+
+var i=str.indexOf("aan");
+
+document.write("<br>"+i);
+
+var j=str.lastIndexOf("Hassaan");
+
+document.write("<br>"+j);
+
+var k=str.replace("Hassaan","Uppal");
+
+document.write("<br>"+k);
+
+var l=str.charCodeAt('A');
+
+document.write("<br>"+l);
+
+var m=String.fromCharCode(97);
+
+document.write("<br>"+m);
